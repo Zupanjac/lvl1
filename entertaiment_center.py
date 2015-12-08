3ee75b320fa6841b924569504c1879fe085036be
Zupanjac <luka.ivkic@gmail.com>
2015-12-08 21:57:19 +0100

    First submit

--- entertaiment_center.py
+++ entertaiment_center.py
@@ -3,26 +3,32 @@ import media
 
 # create the movise that will be shown on the page
 the_godfather = media.Movie("The Godfather",
-						"The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
-					    "https://upload.wikimedia.org/wikipedia/en/1/1c/Godfather_ver1.jpg?download",
-						"https://www.youtube.com/watch?v=sY1S34973zA")
+	"The aging patriarch of an organized crime dynasty transfers "
+	"control of his clandestine empire to his reluctant son.",
+	"https://upload.wikimedia.org/wikipedia/en/1/1c/Godfather_ver1.jpg?download",
+	"https://www.youtube.com/watch?v=sY1S34973zA")
 
 avatar = media.Movie("Avatar",
-					"A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
-					"https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
-					"https://www.youtube.com/watch?v=5PSNL1qE6VY")
+	"A paraplegic marine dispatched to the moon Pandora on unique "
+	"mission becomes torn between following his orders and protecting "
+	"the world he feels is his home.",
+	"https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+	"https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
 the_matrix = media.Movie("The Matrix",
-					"A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
-					"https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",
-					"https://www.youtube.com/watch?v=m8e-FF8MsqU")
+	"A computer hacker learns from mysterious rebels about the true nature "
+	"of his reality and his role in the war against its controllers.",
+	"https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",
+	"https://www.youtube.com/watch?v=m8e-FF8MsqU")
 
 the_shawshank_redemption = media.Movie("The Shawshank Redemption",
-					"Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
-					"https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg",
-					"https://www.youtube.com/watch?v=6hB3S9bIaco")
+	"Two imprisoned men bond over a number of years "
+	"finding solace and eventual redemption through acts of common decency.",
+	"https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg",
+	"https://www.youtube.com/watch?v=6hB3S9bIaco")
 
+#add to an array
 movies = [the_godfather, avatar, the_matrix, the_shawshank_redemption]
 
-#open webbrowser with movies 
+#open webbrowser with preloaded page
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,13 +1,17 @@
 import webbrowser
-# Class
+#This class containes the information about a movie
 class Movie():
-	# lets give the constructor the needed parameter
+	#lets give the constructor the needed parameter
 	def __init__(self, movie_title, move_storyline, poster_image, trailer_youtube):
+		#movie title
 		self.title = movie_title
+		#short describition of the story line
 		self.storyline = move_storyline
+		#poster image of the movie if avaiable
 		self.poster_image_url = poster_image
+		#link to the trailer or the movie
 		self.trailer_youtube_url = trailer_youtube
 
-	# this method opens the trailer for the movie
+	#call this function to open webbrowser with url
 	def show_trailer(self):
 		webbrowser.open(self.trailer_url)
